6ac36f846fbce772b66fc48fadfd2cd6d2236a53
MCMBen <fleafa@gmail.com>
2024-03-15 00:14:26 +0000

    Quartz sync: Mar 15, 2024, 12:14 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -27,7 +27,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#b9beb8", //b9beb8
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
